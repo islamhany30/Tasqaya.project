85b9b83e840967b5005e7424e4eff4664e4d34fb
mohamed <Mamohamed77@outlook.com>
2026-04-24 21:20:45 +0300

    Revert "Fixed a response message regarding reset user password"
    This reverts commit 906798b6295425b9022c4f835c2962f2b283c941.
    
    Revert commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,7 +41,6 @@ import { TaskModule } from './AllModules/Task/Task.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env.development',
     }),
 
     ScheduleModule.forRoot(),
@@ -81,22 +80,20 @@ import { TaskModule } from './AllModules/Task/Task.module';
         logging: true,
         ssl: {
           rejectUnauthorized: false,
-        },
+        }
       }),
     }),
 
     BullModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        connection: config.get<string>('REDIS_URL')
-          ? {
-              url: config.get<string>('REDIS_URL'), // BullMQ بيفهم الـ URL لو بعته كدة
-            }
-          : {
-              host: config.get<string>('REDISHOST') || 'localhost',
-              port: config.get<number>('REDISPORT') || 6379,
-              password: config.get<string>('REDISPASSWORD'),
-            },
+       connection: config.get<string>('REDIS_URL') ? {
+        url: config.get<string>('REDIS_URL') // BullMQ بيفهم الـ URL لو بعته كدة
+        } : {
+          host: config.get<string>('REDISHOST') || 'localhost',
+          port: config.get<number>('REDISPORT') || 6379,
+          password: config.get<string>('REDISPASSWORD'),
+        },
       }),
     }),
 
