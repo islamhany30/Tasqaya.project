82d3eee3537e70841047eca1ebe7e5d8b889556c
islamhany30 <89989239+islamhany30@users.noreply.github.com>
2026-05-30 10:47:55 -0700

    Refactor PayInvoiceDto validation decorators

--- src/AllModules/Company/Dto/PayInvoice.dto.ts
+++ src/AllModules/Company/Dto/PayInvoice.dto.ts
@@ -1,8 +1,13 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { PaymentMethodEnum } from '../../../Enums/payment-method.enum';
+import { PaymentStepEnum } from '../../../Enums/payment-step.enum';
 
 export class PayInvoiceDto {
-  @IsEnum(PaymentMethodEnum, { message: 'Invalid payment method' })
-  @IsNotEmpty({ message: 'Payment method is required' })
+  @IsEnum(PaymentMethodEnum)
+  @IsNotEmpty()
   method: PaymentMethodEnum;
+
+  @IsEnum(PaymentStepEnum)
+  @IsNotEmpty()
+  step: PaymentStepEnum;
 }
