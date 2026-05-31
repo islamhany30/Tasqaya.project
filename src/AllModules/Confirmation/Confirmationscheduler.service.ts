26f70461f7e8f4ef8c7d4725c6ed91b2a1106134
islamhany30 <89989239+islamhany30@users.noreply.github.com>
2026-05-31 15:09:47 -0700

    Change HOURS_BEFORE_EVENT from 1 to 48

--- src/AllModules/Confirmation/Confirmationscheduler.service.ts
+++ src/AllModules/Confirmation/Confirmationscheduler.service.ts
@@ -17,7 +17,7 @@ export class ConfirmationSchedulerService {
   private readonly logger = new Logger(ConfirmationSchedulerService.name);
 
   // private readonly HOURS_BEFORE_EVENT = 48;
-    private readonly HOURS_BEFORE_EVENT = 1;
+    private readonly HOURS_BEFORE_EVENT = 48;
   private readonly SCAN_WINDOW_MINUTES = 60;
 
   constructor(
