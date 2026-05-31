471f354d3715a28092912de161ca5083e0114f26
Mohamed Koura <mohamedkouraa@outlook.com>
2026-06-01 00:07:20 +0300

    Change job post deadline calculation
    Updated job post creation logic to set deadline to 5 minutes from now instead of 48 hours before task start date.

--- src/AllModules/Task/Task.service.ts
+++ src/AllModules/Task/Task.service.ts
@@ -639,8 +639,8 @@ export class TaskService {
     const publishedAt = new Date();
 
     // const deadline = new Date(task.startDate);
+    // deadline.setHours(deadline.getHours() - 48);
     const deadline = new Date(Date.now() + 5 * 60 * 1000);
-    deadline.setHours(deadline.getHours() - 48);
 
     const jobPost = this.jobPostRepo.create({
       task: task,
